159ce67eee22c134bb969756446a087bb9138bc9
Naveed-Ahmad1212 <87361421+Naveed-Ahmad1212@users.noreply.github.com>
2023-02-03 15:55:07 +0500

    Height set of Alert

--- src/Components/Alerts.js
+++ src/Components/Alerts.js
@@ -4,9 +4,9 @@ function Alerts(props) {
   return (
     <div style={{height: '50px'}}>
     
-    {props.alert} && <div className="alert alert-success alert-dismissible fade show" role="alert">
+    {props.alert && <div className="alert alert-success alert-dismissible fade show" role="alert">
   <strong>{props.alert.type}</strong> {props.alert.msg}
-</div>
+</div>}
 </div>
   )
 }
